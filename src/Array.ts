30f2562f038eaebdc95b0c68caaf14aea7e9f6c6
Zach-ma <alexma4cv@gmail.com>
2020-01-15 12:57:07 +0800

    change: export as export func

--- src/Array.ts
+++ src/Array.ts
@@ -1,3 +1,13 @@
-export default class ZArray {
-
+/**
+ * 从数组中按特定方法移除元素，并返回删除这些元素后的新数组
+ * @param arr input arr
+ * @param func remove func
+ */
+export function removeWithFunc(arr: any[], func): any[] {
+    Array.isArray(arr)
+        ? arr.filter(func).reduce((acc, val) => {
+            arr.splice(arr.indexOf(val), 1);
+            return acc.concat(val);
+        }, [])
+        : [];
 }
--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,8 @@
-import ZNumber from '../src/Number'
+import {formatFloatNum,addFloatNum, to64bitFloat}from '../src/Number'
 
-console.log(ZNumber.formatFloatNum(0.332000000000001))
-console.log(ZNumber.addFloatNum(0.1, 0.2))
-console.log(ZNumber.to64bitFloat(-123133231))
+console.log(formatFloatNum(0.332000000000001))
+console.log(addFloatNum(0.1, 0.2))
+console.log(to64bitFloat(-123133231))
+
+import {removeWithFunc} from  "../src/Array"
+console.log(removeWithFunc([1,2,3],function(n){return n >2}))
